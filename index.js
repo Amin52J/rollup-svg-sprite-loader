24624cc19e4518377280664192227efe46f4274d
amin.jafari <amin.jafari@aroundhome.de>
2021-05-07 15:18:25 +0200

    Removes DOMContentLoaded check

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { readFileSync }  = require('fs');
+const {readFileSync} = require('fs');
 const SVGCompiler = require('svg-baker');
 const nanoid = require('nanoid');
 
@@ -11,54 +11,55 @@ const extension = '.svg'
 let svgCompiler = new SVGCompiler();
 const svgReplaceObjects = [];
 
-module.exports = function svgSpriteLoader (options = {}) {
-  return {
-    name: 'rollup-svg-sprite-loader',
-    async load (id) {
-      if (path.extname(id) !== extension)
-        return null;
+module.exports = function svgSpriteLoader(options = {}) {
+    return {
+        name: 'rollup-svg-sprite-loader',
+        async load(id) {
+            if (path.extname(id) !== extension)
+                return null;
 
-      const data = readFileSync( id, 'UTF-8' );
+            const data = readFileSync(id, 'UTF-8');
 
-      let svgReplace = svgReplaceObjects.find(svg => svg.content === data);
-      if (!svgReplace) {
-        const useId = nanoid();
-        const symbol = await svgCompiler.addSymbol({ id: useId, content: data, path: id });
-        svgReplace = {
-            id: JSON.stringify(useId),
-            content: data,
-            viewBox: symbol.viewBox,
-        };
-          svgReplaceObjects.push(svgReplace)
-      }
+            let svgReplace = svgReplaceObjects.find(svg => svg.content === data);
+            if (!svgReplace) {
+                const useId = nanoid();
+                const symbol = await svgCompiler.addSymbol({id: useId, content: data, path: id});
+                svgReplace = {
+                    id: JSON.stringify(useId),
+                    content: data,
+                    viewBox: symbol.viewBox,
+                };
+                svgReplaceObjects.push(svgReplace)
+            }
 
-      // eslint-disable-next-line max-len
-      return`var img = {id: ${svgReplace.id}, content: ${JSON.stringify(svgReplace.content)}, viewbox: ${JSON.stringify(svgReplace.viewBox)}, viewBox: ${JSON.stringify(svgReplace.viewBox)} }; export default img;`;
+            // eslint-disable-next-line max-len
+            return `var img = {id: ${svgReplace.id}, content: ${JSON.stringify(svgReplace.content)}, viewbox: ${JSON.stringify(svgReplace.viewBox)}, viewBox: ${JSON.stringify(svgReplace.viewBox)} }; export default img;`;
 
-      const ast = {
-        type: 'Program',
-        sourceType: 'module',
-        start: 0,
-        end: null,
-        body: []
-      };
+            const ast = {
+                type: 'Program',
+                sourceType: 'module',
+                start: 0,
+                end: null,
+                body: []
+            };
 
-      return { ast, code, map: { mappings: '' } };
+            return {ast, code, map: {mappings: ''}};
 
-    },
-    async renderChunk (code, chunk, options) {
-      const sprite = await Sprite.create({
-        symbols: svgCompiler.symbols,
-        factory: spriteFactory
-      });
-      const content = sprite.render();
-      // eslint-disable-next-line max-len
-      const svgNodeInsert = `function loadSpriteFromRollupSVGSpriteLoader() { if (typeof window !== "undefined" && window && window.document && window.document.body) { window.document.addEventListener('DOMContentLoaded', function () { const div = document.createElement('div'); div.setAttribute('style', 'position: absolute; height:0; width: 0; overflow: hidden;'); div.innerHTML = ${JSON.stringify(content)}; window.document.body.appendChild(div) }); } else { setTimeout(() => { loadSpriteFromRollupSVGSpriteLoader(); }, 50); } } loadSpriteFromRollupSVGSpriteLoader();`;
-      const replacedCode = code + svgNodeInsert;
+        },
+        async renderChunk(code, chunk, options) {
+            const sprite = await Sprite.create({
+                symbols: svgCompiler.symbols,
+                factory: spriteFactory
+            });
+            const content = sprite.render();
 
-      return { code: replacedCode }
-    }
+            // eslint-disable-next-line max-len
+            const svgNodeInsert = `function loadSpriteFromRollupSVGSpriteLoader() { if (typeof window !== "undefined" && window && window.document && window.document.body) { const div = document.createElement('div'); div.setAttribute('style', 'position: absolute; height:0; width: 0; overflow: hidden;'); div.innerHTML = ${JSON.stringify(content)}; window.document.body.appendChild(div); } else { setTimeout(() => { loadSpriteFromRollupSVGSpriteLoader(); }, 50); } } loadSpriteFromRollupSVGSpriteLoader();`;
+            const replacedCode = code + svgNodeInsert;
+
+            return {code: replacedCode}
+        }
 
 
-  };
+    };
 }
